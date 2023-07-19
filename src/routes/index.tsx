a13ce129f538c228cfc1fde1e8e6998d2ba5ffcc
Maria Khomenko <mariakhomenko@google.com>
2023-07-19 16:39:16 -0700

    Partial sign in

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,19 @@
-import { Title } from 'solid-start';
+import { Title, useRouteData } from 'solid-start';
+import { login } from '~/lib/github/discussions';
+import { createServerData$ } from 'solid-start/server';
+import { signIn } from "@solid-auth/base/client";
+
+function CallSignIn() {
+	signIn("gihub", { redirectTo: "/oauth/callback" });
+}
+
 
 export default function Home() {
 	return (
 		<main>
 			<Title>Home</Title>
 			Welcome!
+			<button onClick={CallSignIn}>Sign In</button>
 		</main>
 	);
 }
